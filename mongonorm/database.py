193e1f4166e2988a0ee819db8342ac32f8f7f4a1
crows <pt.wenhan@gmail.com>
2017-04-04 20:47:56 +0800

    finished

--- mongonorm/database.py
+++ mongonorm/database.py
@@ -1,7 +1,23 @@
-class DataBase(object):
-    def __init__(self, mongoclient, name):
-        pass
+from mongonorm import collection
 
-    def collection(self, name):
-        collec = self.db[name]
-        pass
+
+class DataBase(object):
+    def __init__(self, orig_database):
+        self.orig_database = orig_database
+
+    def __getattr__(self, name):
+        return getattr(self.orig_database, name)
+
+    def register(self, name):
+        collec = self.orig_database[name]
+
+        def decorator(cls):
+            cls.__collection__ = collec
+            for method in collection.property_methods:
+                cls.__dict__[method] = property(getattr(collection, method))
+            for method in collection.class_methods:
+                cls.__dict__[method] = classmethod(getattr(collection, method))
+            for method in collection.normal_methods:
+                cls.__dict__[method] = getattr(collection, method)
+            return cls
+        return decorator
---
+++ mongonorm/mongo_client.py
@@ -0,0 +1,26 @@
+from pymongo import MongoClient as OrigMongoClient
+from pymongo.database import DataBase as OrigDataBase
+
+from mongonorm.database import DataBase
+
+
+class MongoClient(object):
+    def __init__(self, host=None, port=None, tz_aware=None,
+                 connect=None, **kwargs):
+        self.mongo_client = OrigMongoClient(host, port, dict, tz_aware,
+                                            connect, kwargs)
+
+    def __repr__(self):
+        return self.mongo_client.__repr__()
+
+    def __getattr__(self, name):
+        rtn = self.mongo_client.__getattr__(name)
+        if isinstance(rtn, OrigDataBase) is True:
+            return DataBase(rtn)
+        return rtn
+
+    def __getitem__(self, name):
+        rtn = self.mongo_client.__getitem__(name)
+        if isinstance(rtn, OrigDataBase) is True:
+            return DataBase(rtn)
+        return rtn
